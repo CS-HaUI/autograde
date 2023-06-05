2ad693a49a9d785e2c653430d58af0ec948c6405
nguyenvanvutlv <nguyenvanvu.tlv@gmail.com>
2023-06-05 16:40:07 +0700

    update

--- main.py
+++ main.py
@@ -4,23 +4,19 @@ import matplotlib.pyplot as plt
 from lib.threshold import auto_threshold
 from lib.morphological import change_brightness, erode, dilate
 
-image = cv2.imread("dataset\\IMAGE\\001.jpg")
+image = cv2.imread("dataset\\IMAGE\\002.jpg")
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 
 gray = cv2.bilateralFilter(gray, 20, 70, 70)
 
-img = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 4)
-edges_image = cv2.morphologyEx(img, cv2.MORPH_CLOSE, np.ones((5, 5)), iterations= 6)
+img = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 151, 4)
 
+edges_image = cv2.morphologyEx(img, cv2.MORPH_CLOSE, np.ones((5, 5)), iterations= 5)
 
-contours, hierarchy = cv2.findContours(edges_image.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-
+# contours, hierarchy = cv2.findContours(edges_image.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 # print(contours)
-print(hierarchy)
-
 
 plt.imshow(edges_image, cmap='gray')
 plt.axis('off')
